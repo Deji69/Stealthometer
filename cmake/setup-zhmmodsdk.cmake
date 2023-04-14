911505a4e6fea0301f2e7f8fe6c00a688d7d01e5
Deji <the_zone@hotmail.co.uk>
2023-04-14 19:45:07 +0100

    SDK update

--- cmake/setup-zhmmodsdk.cmake
+++ cmake/setup-zhmmodsdk.cmake
@@ -1,9 +1,16 @@
-include(FetchContent)
-cmake_policy(SET CMP0135 NEW)
-FetchContent_Declare(
-    ZHMModSDK
-    URL https://github.com/OrfeasZ/ZHMModSDK/releases/download/${ZHMMODSDK_VER}/DevPkg-ZHMModSDK.zip
-)
+# If ZHMMODSDK_DIR is set, use that. Otherwise, download the SDK from GitHub.
+if (DEFINED ZHMMODSDK_DIR)
+    include("${ZHMMODSDK_DIR}/cmake/sdk-local.cmake")
+else()
+    include(FetchContent)
+    cmake_policy(SET CMP0135 NEW)
+    FetchContent_Declare(
+        ZHMModSDK
+        URL https://github.com/OrfeasZ/ZHMModSDK/releases/download/${ZHMMODSDK_VER}/DevPkg-ZHMModSDK.zip
+    )
 
-FetchContent_MakeAvailable(ZHMModSDK)
-include("${zhmmodsdk_SOURCE_DIR}/cmake/zhm-mod.cmake")
+    FetchContent_MakeAvailable(ZHMModSDK)
+    set(ZHMMODSDK_DIR "${zhmmodsdk_SOURCE_DIR}")
+endif()
+
+include("${ZHMMODSDK_DIR}/cmake/zhm-mod.cmake")
